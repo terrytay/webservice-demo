1f88183c7e2b9139e397c8af122d16d1af1146df
terrytay <txrrythk@gmail.com>
2020-04-23 10:08:27 +0800

    make REST API stateless and read token secret from property file

--- src/main/java/com/terry/webservice/demo/DemoApplication.java
+++ src/main/java/com/terry/webservice/demo/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.terry.webservice.demo.security.AppProperties;
+import com.terry.webservice.demo.service.impl.UserServiceImpl;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -21,4 +24,10 @@ public class DemoApplication {
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
+	
+	@Bean
+	public AppProperties getAppProperties() {
+		return new AppProperties();
+	}
+	
 }
